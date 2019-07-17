68da0ff835302424166692cd91b0032042bd2593
小久保洋平 <yheihei0126@gmail.com>
2019-07-17 22:43:09 +0900

    テスト環境構築

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Draft_Portfolio_child
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
