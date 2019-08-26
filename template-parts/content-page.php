18efa009987ce7a1f67ed64df3f6c83179f5ff20
小久保洋平 <yheihei0126@gmail.com>
2019-08-26 23:58:35 +0900

    リファクタリング 記事の構造化データに対応する(#7)

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,27 +10,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>  itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
-	<meta itemprop='author' content="<?php the_author(); ?>">
-	<meta itemprop='description' content="<?php echo get_the_excerpt(); ?>">
-	<meta itemprop='datePublished' content="<?php echo get_date_from_gmt(get_post_time('c', true), 'c');?>">
-	<meta itemprop='dateModified' content="<?php echo get_date_from_gmt(get_post_modified_time('c', true), 'c');?>">
-	<div itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
-		<?php if( has_post_thumbnail() ) : ?>
-		<meta itemprop="url" content="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'full' ); ?>">
-		<?php else: ?>
-		<meta itemprop="url" content="<?php echo get_stylesheet_directory_uri(); ?>/img/yhei_web_design_catch-800x640.jpg">
-		<?php endif; ?>		
-	</div>
-	<meta itemscope itemprop="mainEntityOfPage"  itemType="https://schema.org/WebPage" itemid="<?php the_permalink(); ?>"/>
-	<div itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-		<div itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
-			<meta itemprop="url" content="<?php echo get_stylesheet_directory_uri(); ?>/img/yhei_web_design_catch-800x640.jpg">
-			<meta itemprop="width" content="800">
-			<meta itemprop="height" content="640">
-		</div>
-		<meta itemprop="name" content="<?php bloginfo( 'name' ) ?>">
-	</div>
-	<meta itemprop="headline" content="<?php the_title(); ?>">
+	<?php the_blog_post(); ?>
 	<div class='post-thumb'>
 		<?php the_post_thumbnail('full'); ?>
 	</div>
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,35 +10,16 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting" >
-		<meta itemprop='author' content="<?php the_author(); ?>">
-		<meta itemprop='description' content="<?php echo get_the_excerpt(); ?>">
-		<meta itemprop='datePublished' content="<?php echo get_date_from_gmt(get_post_time('c', true), 'c');?>">
-		<meta itemprop='dateModified' content="<?php echo get_date_from_gmt(get_post_modified_time('c', true), 'c');?>">
-		<div class='post-thumb' itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
-				
-				<?php the_post_thumbnail('full'); ?>
-				<?php if( has_post_thumbnail() ) : ?>
-				<meta itemprop="url" content="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'full' ); ?>">
-				<?php else: ?>
-				<meta itemprop="url" content="<?php echo get_stylesheet_directory_uri(); ?>/img/yhei_web_design_catch-800x640.jpg">
-				<?php endif; ?>
-				
-		</div>
-		<meta itemscope itemprop="mainEntityOfPage"  itemType="https://schema.org/WebPage" itemid="<?php the_permalink(); ?>"/>
-		<div itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-			<div itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
-				<meta itemprop="url" content="<?php echo get_stylesheet_directory_uri(); ?>/img/yhei_web_design_catch-800x640.jpg">
-				<meta itemprop="width" content="800">
-				<meta itemprop="height" content="640">
-			</div>
-			<meta itemprop="name" content="<?php bloginfo( 'name' ) ?>">
-		</div>
+	<?php the_blog_post(); ?>
+	<div class='post-thumb'>
+			<?php the_post_thumbnail('full'); ?>
+	</div>
 	<header class="entry-header">
 		<?php
 		if ( is_single() ) :
-			the_title( '<h1 class="entry-title" itemprop="headline">', '</h1>' );
+			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
-			the_title( '<h2 class="entry-title" itemprop="headline"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark" itemprop="mainEntityOfPage">', '</a></h2>' );
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
