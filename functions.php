efbc61c18f94d6c6541a3129235735cbb4da2dc0
小久保洋平 <yheihei0126@gmail.com>
2019-09-23 20:41:10 +0900

    レビュー指摘修正 投稿者の表示を消したい(#12) https://bitbucket.org/yheihei/wp-yhei-web-design/pull-requests/14/12/diff#comment-117906103

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_parent_theme_file_uri() . '/style.css' );
     // Theme stylesheet.
-  wp_enqueue_style( 'draft-portfolio-style', get_stylesheet_uri(), null, filemtime( get_stylesheet_directory() . '/style.css'), null );
+	wp_enqueue_style( 'draft-portfolio-style', get_stylesheet_uri(), null, filemtime( get_stylesheet_directory() . '/style.css'), null );
 }
 
 // 親カテゴリーのアーカイブを子カテゴリーも使う
@@ -55,27 +55,27 @@ function createDiaryPostsQuery() {
  */
 function the_blog_post() {
   ?>
-  <meta itemprop='author' content="<?php the_author(); ?>">
-  <meta itemprop='description' content="<?php echo get_the_excerpt(); ?>">
-  <meta itemprop='datePublished' content="<?php echo get_date_from_gmt(get_post_time('c', true), 'c');?>">
-  <meta itemprop='dateModified' content="<?php echo get_date_from_gmt(get_post_modified_time('c', true), 'c');?>">
-  <div itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
-    <?php if( has_post_thumbnail() ) : ?>
-    <meta itemprop="url" content="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'full' ); ?>">
-    <?php else: ?>
-    <meta itemprop="url" content="<?php echo get_stylesheet_directory_uri(); ?>/img/yhei_web_design_catch-800x640.jpg">
-    <?php endif; ?>		
-  </div>
-  <meta itemscope itemprop="mainEntityOfPage"  itemType="https://schema.org/WebPage" itemid="<?php the_permalink(); ?>"/>
-  <div itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-    <div itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
-      <meta itemprop="url" content="<?php echo get_stylesheet_directory_uri(); ?>/img/yhei_web_design_catch-800x640.jpg">
-      <meta itemprop="width" content="800">
-      <meta itemprop="height" content="640">
-    </div>
-    <meta itemprop="name" content="<?php bloginfo( 'name' ) ?>">
-  </div>
-  <meta itemprop="headline" content="<?php the_title(); ?>">
+	<meta itemprop='author' content="<?php the_author(); ?>">
+	<meta itemprop='description' content="<?php echo get_the_excerpt(); ?>">
+	<meta itemprop='datePublished' content="<?php echo get_date_from_gmt(get_post_time('c', true), 'c');?>">
+	<meta itemprop='dateModified' content="<?php echo get_date_from_gmt(get_post_modified_time('c', true), 'c');?>">
+	<div itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
+		<?php if( has_post_thumbnail() ) : ?>
+		<meta itemprop="url" content="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'full' ); ?>">
+		<?php else: ?>
+		<meta itemprop="url" content="<?php echo get_stylesheet_directory_uri(); ?>/img/yhei_web_design_catch-800x640.jpg">
+		<?php endif; ?>		
+	</div>
+	<meta itemscope itemprop="mainEntityOfPage"  itemType="https://schema.org/WebPage" itemid="<?php the_permalink(); ?>"/>
+	<div itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+		<div itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
+			<meta itemprop="url" content="<?php echo get_stylesheet_directory_uri(); ?>/img/yhei_web_design_catch-800x640.jpg">
+			<meta itemprop="width" content="800">
+			<meta itemprop="height" content="640">
+		</div>
+		<meta itemprop="name" content="<?php bloginfo( 'name' ) ?>">
+	</div>
+	<meta itemprop="headline" content="<?php the_title(); ?>">
 <?php  
 }
 
