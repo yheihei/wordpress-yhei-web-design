8e311c0b9b536c72c4472a2f7f1b06f7c6a9786e
小久保洋平 <yheihei0126@gmail.com>
2019-11-13 13:07:51 +0900

    archiveページを全てタイル形式の記事表示にする #6

--- template-parts/content-preview.php
+++ template-parts/content-preview.php
@@ -8,28 +8,39 @@
  */
 
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'col-4-12 grid-item mobile-col-6-12 small-col-1'); ?> itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
+<article id="post-<?php the_ID(); ?>" <?php 
+global $template; // テンプレートファイルのパスを取得
+$template_name = basename($template);
+if( is_archive() && $template_name === 'archive.php' ) {
+	// アーカイブページの場合はサイドバーありのgrid表示クラスにする
+	post_class( 'grid-item yhei-grid-item col-6-12-custom mobile-col-6-12 small-col-1');
+} else {
+	post_class( 'col-4-12 grid-item mobile-col-6-12 small-col-1');
+}
+?> itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
 	<?php the_blog_post(); ?>
-	<?php 	if ( has_post_thumbnail() ) { ?>
-		<div class='post-thumb'>
-				<a href="<?php the_permalink();?>" >
-				<?php the_post_thumbnail('draft-portfolio-thumbnail'); ?>
-				</a>
-		</div>
-		<div class='post-title'>
-			<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
-			
-			<p class="date">
-				<?php the_time('Y.m.d'); ?>
-			</p>
-		</div>
+	<div class='post-thumb'>
+		<a href="<?php the_permalink();?>" >
+		<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail('draft-portfolio-thumbnail'); ?>
+		<?php else: ?>
+			<img width="800" height="640" src="<?php echo get_stylesheet_directory_uri(); ?>/img/yhei_web_design_catch-800x640.jpg" class="attachment-draft-portfolio-thumbnail size-draft-portfolio-thumbnail wp-post-image" alt="<?= $category_name ?>" />
+		<?php endif; ?>
+		</a>
+	</div>
+	<div class='post-title'>
+		<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+		
+		<p class="date">
+			<?php the_time('Y.m.d'); ?>
+		</p>
+	</div>
 
-  <div class="overbox">
-    <div class="title overtext"> 
-    	<?php //the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+<div class="overbox">
+	<div class="title overtext"> 
+		<?php //the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
 </div>
-    <div class="tagline overtext"> <?php draft_portfolio_category();?> </div>
-  </div>
+	<div class="tagline overtext"> <?php draft_portfolio_category();?> </div>
+</div>
 
-	<?php } ?>
 </article><!-- #post-## -->
