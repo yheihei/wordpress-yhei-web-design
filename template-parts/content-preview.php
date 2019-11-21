a966bce598e61a306f4215880c65e18a12169d60
小久保洋平 <yheihei0126@gmail.com>
2019-11-21 09:26:37 +0900

    記事一覧で一覧の高さが不定の場合に綺麗に横並びにならない #10

--- template-parts/content-preview.php
+++ template-parts/content-preview.php
@@ -13,18 +13,19 @@ global $template; // テンプレートファイルのパスを取得
 $template_name = basename($template);
 if( is_archive() && $template_name === 'archive.php' ) {
 	// アーカイブページの場合はサイドバーありのgrid表示クラスにする
-	post_class( 'grid-item yhei-grid-item col-6-12-custom mobile-col-6-12 small-col-1');
+	// post_class( 'grid-item yhei-grid-item col-6-12-custom mobile-col-6-12 small-col-1');
+	post_class( 'yhei-grid__item yhei-grid__item--primary yhei-post');
 } else {
 	post_class( 'col-4-12 grid-item mobile-col-6-12 small-col-1');
 }
 ?> itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
 	<?php the_blog_post(); ?>
-	<div class='post-thumb'>
+	<div class='post-thumb yhei-post__thumbnail'>
 		<a href="<?php the_permalink();?>" >
 		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail('draft-portfolio-thumbnail'); ?>
+			<?php the_post_thumbnail('draft-portfolio-thumbnail', [ 'class' => 'yhei-post__eyecatch' ] ); ?>
 		<?php else: ?>
-			<img width="800" height="640" src="<?php echo get_stylesheet_directory_uri(); ?>/img/yhei_web_design_catch-800x640.jpg" class="attachment-draft-portfolio-thumbnail size-draft-portfolio-thumbnail wp-post-image" alt="<?= $category_name ?>" />
+			<img class="yhei-post__eyecatch" width="800" height="640" src="<?php echo get_stylesheet_directory_uri(); ?>/img/yhei_web_design_catch-800x640.jpg" class="attachment-draft-portfolio-thumbnail size-draft-portfolio-thumbnail wp-post-image" alt="<?= $category_name ?>" />
 		<?php endif; ?>
 		</a>
 	</div>
--- template-parts/content-preview_category.php
+++ template-parts/content-preview_category.php
@@ -14,39 +14,41 @@
 $current_term = get_current_term();
 ?>
 <?php if( $current_term->description ) : ?>
-<article id="post-<?= $current_term->term_id ?>" <?php post_class(); ?> >
+<article id="post-<?= $current_term->term_id ?>" <?php post_class(''); ?> >
   <div class="entry-content entry-content--category">
     <?php echo $current_term->description; ?>
   </div><!-- .entry-content -->
 </article>
 <?php endif; ?>
-<?php
-// 表示されているカテゴリーの直近1つの子カテゴリーの情報を取得
-$child_categorys = get_child_categorys($current_term->term_id);
-foreach( $child_categorys as $child_category ) :
-  $child_theme_uri  = get_stylesheet_directory_uri();
-  $category_id   = $child_category->term_id;
-  $category_name = $child_category->name;
-  $category_slug = $child_category->slug;
-  $category_url = get_category_link($category_id);
-  $category_eyecatch_url = get_category_eyecatch( $category_id );
-?>
-  <article id="post-<?= $category_id ?>" <?php post_class( 'grid-item yhei-grid-item col-6-12-custom mobile-col-6-12 small-col-1'); ?>>
-    <div class='post-thumb'>
-        <a href="<?php echo $category_url;?>" >
-          <?php if($category_eyecatch_url) :  ?>
-            <img src="<?php echo $category_eyecatch_url; ?>" class="attachment-draft-portfolio-thumbnail size-draft-portfolio-thumbnail wp-post-image" alt="<?= $category_name ?>" />
-          <?php else: ?>
-            <img width="800" height="640" src="<?php echo $child_theme_uri; ?>/img/yhei_web_design_catch-800x640.jpg" class="attachment-draft-portfolio-thumbnail size-draft-portfolio-thumbnail wp-post-image" alt="<?= $category_name ?>" />
-          <?php endif; ?>
+<div class="yhei-grid">
+  <?php
+  // 表示されているカテゴリーの直近1つの子カテゴリーの情報を取得
+  $child_categorys = get_child_categorys($current_term->term_id);
+  foreach( $child_categorys as $child_category ) :
+    $child_theme_uri  = get_stylesheet_directory_uri();
+    $category_id   = $child_category->term_id;
+    $category_name = $child_category->name;
+    $category_slug = $child_category->slug;
+    $category_url = get_category_link($category_id);
+    $category_eyecatch_url = get_category_eyecatch( $category_id );
+  ?>
+    <article id="post-<?= $category_id ?>" <?php post_class( 'yhei-grid__item yhei-grid__item--primary yhei-post'); ?>>
+      <div class='post-thumb yhei-post__thumbnail'>
+          <a href="<?php echo $category_url;?>" >
+            <?php if($category_eyecatch_url) :  ?>
+              <img src="<?php echo $category_eyecatch_url; ?>" class="yhei-post__eyecatch" alt="<?= $category_name ?>" />
+            <?php else: ?>
+              <img width="800" height="640" src="<?php echo $child_theme_uri; ?>/img/yhei_web_design_catch-800x640.jpg" class="yhei-post__eyecatch" alt="<?= $category_name ?>" />
+            <?php endif; ?>
+          </a>
+      </div>
+      <div class='post-title'>
+      <h2 class="entry-title">
+        <a href="<?php echo $category_url; ?>" rel="bookmark">
+        <?php echo $category_name; ?>
         </a>
-    </div>
-    <div class='post-title'>
-    <h2 class="entry-title">
-      <a href="<?php echo $category_url; ?>" rel="bookmark">
-      <?php echo $category_name; ?>
-      </a>
-    </h2>
-    </div>
-  </article><!-- #post-## -->
-<?php endforeach; ?>
+      </h2>
+      </div>
+    </article><!-- #post-## -->
+  <?php endforeach; ?>
+</div>
